6029a9a8519cc0713de8927319158e8ba9768885
SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
2023-12-11 17:34:52 +0100

    Day 9 part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -35,6 +35,30 @@ fn extrapolate(numbers: &Vec<i64>) -> i64 {
     return  new_number;
 }
 
+// Optional boolean decreasing
+fn extrapolate_backwards(numbers: &Vec<i64>) -> i64 {
+    if numbers.len() == 1 {
+        panic!("I fucked up");
+    }
+    println!("Numbers: {:?}", numbers);
+    // Check if all numbers are 0:
+    if ! numbers.iter().any(|&x| x != 0) {
+        return 0
+    }
+
+    let mut new_level: Vec<i64> = Vec::new();
+
+    for i in 0..numbers.len() - 1 {
+        // abs diff
+        let diff = numbers[i + 1] - numbers[i];
+        new_level.insert(0, diff);
+    }
+
+    let new_number = numbers[0] - extrapolate(&new_level);
+    println!("New number: {}", new_number);
+    return  new_number;
+}
+
 fn part_1() {
     let mut result: i64 = 0;
     let mut result_values: Vec<i64> = Vec::new();
@@ -62,13 +86,19 @@ fn part_1() {
 
 
 fn part_2() {
+    let mut result: i64 = 0;
+
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
             if let Ok(row) = line {
-
+                let mut numbers: Vec<i64> = row.split(" ").map(|s| s.parse::<i64>().unwrap()).collect();
+                println!("Numbers: {:?}", numbers);
+                let new_number = extrapolate_backwards(&mut numbers);
+                result += new_number;
             }
         }
     }
+    println!("Result: {}", result);
 }
 
 fn main() {
